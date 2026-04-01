61b4cbb4c990533f13729d1b05753b4f2376e2c0
seif090 <seiftarek090@gmail.com>
2026-04-01 15:08:14 +0200

    Update binaries and assembly info for FreightSystem.Api, FreightSystem.Application, FreightSystem.Core, and FreightSystem.Infrastructure
    - Updated DLLs and PDBs for FreightSystem.Api, FreightSystem.Application, FreightSystem.Core, and FreightSystem.Infrastructure due to changes in the source code.
    - Modified AssemblyInfo.cs files to reflect new informational version.
    - Updated sourcelink.json files to point to the latest commit in the repository.
    - Regenerated cache files for assembly references and inputs.

--- src/FreightSystem.Api/obj/Debug/net10.0/FreightSystem.Api.AssemblyInfo.cs
+++ src/FreightSystem.Api/obj/Debug/net10.0/FreightSystem.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreightSystem.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c70001b6914c77117a16feb1f9e66b23b85e5ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b8377ac9e117220fee88de986589678fd77ad93")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreightSystem.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreightSystem.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
